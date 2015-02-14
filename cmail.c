7fe224c55a4a18d588fa46068b68302972453dcf
cbdev <cb@cbcdn.com>
2015-02-15 00:05:21 +0100

    main skeleton

--- cmail.c
+++ cmail.c
@@ -1,9 +1,46 @@
 #include "cmail.h"
 
-
+int usage(char* filename){
+	printf("%s usage information\n", VERSION);
+	//TODO
+	return EXIT_FAILURE;
+}
 
 int main(int argc, char** argv){
+	ARGUMENTS args = {
+		.config_file = NULL,
+		.drop_privileges = true, 
+		.detach = true
+	};
+
+	//parse arguments
+	if(!arguments_parse(&args, argc-1, argv+1)){
+		arguments_free(&args);
+		exit(usage(argv[1]));
+	}
+	
+	//read config file
+	//TODO
+
 	fprintf(stderr, "This is %s, starting up\n", VERSION);
+
+	//open & attach databases
+	//TODO
+
+	//bind all ports
+	//TODO
+
+	//drop privileges
+	//TODO
+	
+	//detach from console (or dont)
+	//TODO
+	
+	//enter main processing loop
+	//TODO
+	
+	//clean up allocated resources
+	//TODO
 
 	fprintf(stderr, "Bye.");
 	return EXIT_SUCCESS;
--- cmail.h
+++ cmail.h
@@ -6,3 +6,12 @@
 #include <sqlite3.h>
 
 #define VERSION "cmail 0.1"
+
+typedef struct /*_ARGS*/ {
+	char* config_file;
+	bool drop_privileges;
+	bool detach;
+} ARGUMENTS;
+
+#include "arguments.c"
+#include "conffile.c"
