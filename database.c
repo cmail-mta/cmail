a826e2fbba11fcff8f178ff68bd692e3d92c633b
cbdev <cb@cbcdn.com>
2015-02-18 02:15:57 +0100

    database attach skeleton

--- database.c
+++ database.c
@@ -19,6 +19,8 @@ sqlite3_stmt* database_prepare(LOGGER log, sqlite3* master, char* query){
 int database_initialize(LOGGER log, sqlite3* master){
 	char* QUERY_ATTACH_DB="ATTACH DATABASE ? AS ?;";
 	char* QUERY_SELECT_DATABASES="SELECT user_name, user_route FROM users WHERE user_router='store' AND user_route NOT NULL GROUP BY user_route;";
+	int status=SQLITE_ROW;
+	int rv=0;
 
 	sqlite3_stmt* attach_db=database_prepare(log, master, QUERY_ATTACH_DB);
 	sqlite3_stmt* select_dbs=database_prepare(log, master, QUERY_SELECT_DATABASES);
@@ -27,9 +29,25 @@ int database_initialize(LOGGER log, sqlite3* master){
 		return -1;
 	}
 
-	//TODO
+	while(status==SQLITE_ROW){
+		status=sqlite3_step(select_dbs);
+		switch(status){
+			case SQLITE_ROW:
+				logprintf(log, LOG_INFO, "Attaching database %s as %s\n", sqlite3_column_text(select_dbs, 1), sqlite3_column_text(select_dbs, 0));
+				//TODO attach database
+				//TODO compile insert statements
+				break;
+			case SQLITE_DONE:
+				logprintf(log, LOG_INFO, "Database initialization done\n");
+				break;
+			default:
+				logprintf(log, LOG_ERROR, "Database initialization failed: %s\n", sqlite3_errmsg(master));
+				rv=-1;
+				break;
+		}
+	}
 	
 	sqlite3_finalize(attach_db);
 	sqlite3_finalize(select_dbs);
-	return 0;
+	return rv;
 }
