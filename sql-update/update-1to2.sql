6abcb877760843236a6151b71e45fec2c8e43f26
Jan Düpmeier <j.duepmeier@googlemail.com>
2015-03-28 18:38:04 +0100

    better sql update scripts (adding .bail on for exit on error)

--- sql-update/update-1to2.sql
+++ sql-update/update-1to2.sql
@@ -1,6 +1,12 @@
+.bail on
+
 BEGIN TRANSACTION;
-ALTER TABLE mailbox ADD COLUMN mail_read BOOLEAN NOT NULL DEFAULT(0);
-DELETE FROM meta WHERE key='schema_version' AND value='1';
-INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version','2');
+
+	-- update schema version  --
+	DELETE FROM meta WHERE key='schema_version' AND value='1';
+	INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version','2');
+
+	-- database changes --
+	ALTER TABLE mailbox ADD COLUMN mail_read BOOLEAN NOT NULL DEFAULT(0);
 COMMIT;
 
--- sql-update/update-2to3.sql
+++ sql-update/update-2to3.sql
@@ -1,6 +1,12 @@
+.bail on
+
 BEGIN TRANSACTION;
-ALTER TABLE users ADD COLUMN user_database TEXT;
-DELETE FROM meta WHERE key='schema_version' AND value='2';
-INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version', '3');
+
+	-- update schema version --
+	DELETE FROM meta WHERE key='schema_version' AND value='2';
+	INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version', '3');
+
+	-- database changes --
+	ALTER TABLE users ADD COLUMN user_database TEXT;
 COMMIT;
 
