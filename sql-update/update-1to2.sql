370c386af3d59c0de3394c2b794dcf6a6976249d
Jan Düpmeier <j.duepmeier@googlemail.com>
2015-03-28 18:27:28 +0100

    fixing bugs in sql update files

--- sql-update/update-1to2.sql
+++ sql-update/update-1to2.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 ALTER TABLE mailbox ADD COLUMN mail_read BOOLEAN NOT NULL DEFAULT(0);
-DELETE OR ROLLBACK FROM meta WHERE key='schema_version' AND value='1';
-INSERT OR ROLLBACK meta (key, value) VALUES ('schema_version','2');
+DELETE FROM meta WHERE key='schema_version' AND value='1';
+INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version','2');
 COMMIT;
 
--- sql-update/update-2to3.sql
+++ sql-update/update-2to3.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 ALTER TABLE users ADD COLUMN user_database TEXT;
-DELETE OR ROLLBACK FROM meta WHERE key='schema_version' AND value='2';
-INSERT OR ROLLBACK meta (key, value) VALUES ('schema_version', '3');
+DELETE FROM meta WHERE key='schema_version' AND value='2';
+INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version', '3');
 COMMIT;
 
