8058216c4e43892a635f96ea3f0f12798dfe2b6e
Jan Düpmeier <j.duepmeier@googlemail.com>
2015-03-28 18:19:40 +0100

    change update scripts to do a better version check

--- sql-update/update-1to2.sql
+++ sql-update/update-1to2.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 ALTER TABLE mailbox ADD COLUMN mail_read BOOLEAN NOT NULL DEFAULT(0);
-UPDATE OR ROLLBACK meta SET value='2' WHERE key='schema_version' AND value='1';
+DELETE OR ROLLBACK FROM meta WHERE key='schema_version' AND value='1';
+INSERT OR ROLLBACK meta (key, value) VALUES ('schema_version','2');
 COMMIT;
 
--- sql-update/update-2to3.sql
+++ sql-update/update-2to3.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 ALTER TABLE users ADD COLUMN user_database TEXT;
-UPDATE OR ROLLBACK meta SET value='3' WHERE key='schema_version' AND value='2';
+DELETE OR ROLLBACK FROM meta WHERE key='schema_version' AND value='2';
+INSERT OR ROLLBACK meta (key, value) VALUES ('schema_version', '3');
 COMMIT;
 
