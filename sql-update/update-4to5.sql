1fde5f6f13e46e97461fc4823f3339d16d5bba73
Jan Düpmeier <j.duepmeier@googlemail.com>
2015-04-07 15:29:42 +0200

    adding constraints to update file, add insert cmd for adding admin rights for every user (to avoid lockouts)

---
+++ sql-update/update-4to5.sql
@@ -0,0 +1,41 @@
+.bail on
+.echo on
+
+BEGIN TRANSACTION;
+
+	-- update schema version --
+	DELETE FROM meta WHERE key='schema_version' AND value='4';
+	INSERT OR ROLLBACK INTO meta (key, value) VALUES ('schema_version', '5');
+
+	-- database changes --
+	CREATE TABLE api_access (
+		api_user 		TEXT	NOT NULL
+						REFERENCES users ( user_name ) 	ON DELETE CASCADE
+										ON UPDATE CASCADE,
+		api_right		TEXT	NOT NULL,
+		CONSTRAINT api_user_right UNIQUE ( api_user, api_right ) ON CONFLICT FAIL
+	);
+
+	CREATE TABLE api_address_delegates (
+		api_user		TEXT	NOT NULL
+						REFERENCES users ( user_name ) 	ON DELETE CASCADE
+										ON UPDATE CASCADE,
+		api_expression		TEXT	NOT NULL,
+		CONSTRAINT api_user_expression UNIQUE ( api_user, api_expression ) ON CONFLICT FAIL
+	);
+
+	CREATE TABLE api_user_delegates (
+		api_user		TEXT	NOT NULL
+						REFERENCES users ( user_name ) 	ON DELETE CASCADE
+										ON UPDATE CASCADE,
+		api_delegate		TEXT	NOT NULL,
+		CONSTRAINT api_user_delegate UNIQUE ( api_user, api_delegate ) ON CONFLICT FAIL
+	);
+
+
+	-- add admin right to every user (to avoid lock outs) --
+	-- IMPORTANT: remove access to non admin users!!!! --
+	INSERT INTO api_access (api_user, api_right) SELECT user_name as api_user, 'admin' AS api_right FROM users;
+
+COMMIT;
+
