1ba876d39ffac3aa792853a89ef5c40939fb151b
cbdev <cb@cbcdn.com>
2015-04-19 17:59:56 +0200

    implement mta socket read

--- cmail-mta/protocol.c
+++ cmail-mta/protocol.c
@@ -14,6 +14,7 @@ int protocol_reply_reset(LOGGER log, SMTPREPLY* reply){
 int protocol_read(LOGGER log, CONNECTION* conn, int timeout){
 	CONNDATA* conn_data=(CONNDATA*)conn->aux_data;
 	int status;
+	ssize_t bytes, left;
 	fd_set readfds;
 	struct timeval tv;
 	time_t read_begin=time(NULL);
@@ -46,10 +47,80 @@ int protocol_read(LOGGER log, CONNECTION* conn, int timeout){
 		#else
 		if(FD_ISSET(conn->fd, &readfds)){
 		#endif
-			//TODO read from peer
+			//read from peer
+			left=sizeof(conn_data->recv_buffer)-conn_data->recv_offset;
 
+			if(left<2){
+				//unterminated line
+				logprintf(log, LOG_ERROR, "Received response line with %d bytes, bailing out\n", conn_data->recv_offset);
+				return -1;
+			}
+	
+			bytes=network_read(log, conn, conn_data->recv_buffer+conn_data->recv_offset, left);
+
+			//failed to read from socket
+			if(bytes<0){
+				#ifndef CMAIL_NO_TLS
+				switch(conn->tls_mode){
+					case TLS_NONE:
+				#endif
+				switch(errno){
+					case EAGAIN:
+						logprintf(log, LOG_WARNING, "Read signaled, but blocked\n");
+						continue;
+					default:
+						logprintf(log, LOG_ERROR, "Failed to read from client: %s\n", strerror(errno));
+						return -1;
+				}
+				#ifndef CMAIL_NO_TLS
+					break;
+					case TLS_NEGOTIATE:
+						//errors during TLS negotiation
+						if(bytes==-2){
+							logprintf(log, LOG_ERROR, "TLS negotiation failed\n");
+							return -1;
+						}
+						continue;
+					case TLS_ONLY:
+						switch(bytes){
+							case GNUTLS_E_INTERRUPTED:
+							case GNUTLS_E_AGAIN:
+								logprintf(log, LOG_WARNING, "TLS read signaled, but blocked\n");
+								continue;
+							default:
+								logprintf(log, LOG_ERROR, "GnuTLS reported an error while reading: %s\n", gnutls_strerror(bytes));
+								return -1;
+						}
+				}
+				#endif
+			}
+	
+			//client disconnect / handshake success
+			else if(bytes==0){
+				#ifndef CMAIL_NO_TLS
+				switch(conn->tls_mode){
+					case TLS_NEGOTIATE:
+						//tls handshake ok
+						conn->tls_mode=TLS_ONLY;
+						break;
+					default:
+				#endif
+				logprintf(log, LOG_INFO, "Peer disconnected\n");
+				return -1;
+				#ifndef CMAIL_NO_TLS
+				}
+				#endif
+				continue;
+			}
+
+			logprintf(log, LOG_DEBUG, "Received %d bytes of data, recv_offset is %d\n", bytes, conn_data->recv_offset);
+
+			//TODO process read data
+			//read response code
+			//parse message into reply structure
+			//check for multiline
+			//continue if not at end
 		}
-
 	}
 	while(!abort_signaled);
 
