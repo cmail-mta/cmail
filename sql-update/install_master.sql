32e87a560c54fa5b875ba3a5ebe3b38d375e8e43
another <code@deadcode.eu>
2015-05-06 02:53:45 +0200

    update sql-update/install_master.sql to schema version 6

--- sql-update/install_master.sql
+++ sql-update/install_master.sql
@@ -111,5 +111,12 @@ BEGIN TRANSACTION;
 										ON UPDATE CASCADE,
 		CONSTRAINT api_user_delegate UNIQUE ( api_user, api_delegate ) ON CONFLICT FAIL
 	);
-INSERT INTO meta (key, value) VALUES ('schema_version', '5');
+
+	CREATE TABLE faillog (
+		fail_mail	INTEGER NOT NULL REFERENCES outbox (mail_id) ON DELETE CASCADE ON UPDATE CASCADE,
+		fail_message	TEXT,
+		fail_time	TEXT NOT NULL DEFAULT (strftime( '%s', 'now' )),
+		fail_fatal	BOOLEAN NOT NULL DEFAULT (0)
+	);
+INSERT INTO meta (key, value) VALUES ('schema_version', '6');
 COMMIT;
