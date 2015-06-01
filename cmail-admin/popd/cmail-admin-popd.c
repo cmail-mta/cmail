bd40cb9b775e2079d157bb38f1e28e3a214727ff
Jan Düpmeier <j.duepmeier@googlemail.com>
2015-06-01 17:24:41 +0200

    adding admin tool for popd

---
+++ cmail-admin/popd/cmail-admin-popd.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <time.h>
+
+#include "../cmail-admin.h"
+
+// logger support
+#include "../../lib/logger.h"
+#include "../../lib/logger.c"
+
+// database suff
+#include "../../lib/database.h"
+#include "../../lib/database.c"
+
+// common stuff
+#include "../../lib/common.h"
+#include "../../lib/common.c"
+
+#include "popd.c"
+
+void usage() {
+
+
+	printf("cmail-admin-popd: Administration tool for cmail-popd.\n");
+	printf("usage:\n");
+	printf("\t--verbosity, -v\t\t Set verbosity level (0 - 4)\n");
+	printf("\t--dbpath, -d <dbpath>\t path to master database\n");
+	printf("\t--help, -h\t\t shows this help\n");
+	printf("\tadd <user>\t\t enables <user> for pop\n");
+	printf("\tdelete <user> \t disables <user> for pop\n");
+	printf("\tunlock <user> \t  unlocks the given user.\n");
+	printf("\tlist [<user>] list all popd entries or if defined only popd entries like <user>\n");
+	printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+
+	LOGGER log = {
+		.stream = stderr,
+		.verbosity = 0
+	};
+
+	DATABASE database = {
+		.conn = NULL
+	};
+
+	int i, status;
+
+	char* dbpath = "/var/cmail/master.db3";
+
+	for (i = 1; i < argc; i++) {
+
+		if (i + 1 < argc && (!strcmp(argv[i], "--dbpath") || !strcmp(argv[i], "-d"))) {
+			dbpath = argv[i + 1];
+			i++;
+		} else if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h")) {
+			usage();
+			return 0;
+		} else if (i + 1 < argc && (!strcmp(argv[i], "--verbosity") || !strcmp(argv[i], "-v"))) {
+			log.verbosity = strtoul(argv[i + 1], NULL, 10);
+		} else if (i + 1 < argc && (!strcmp(argv[i], "add"))) {
+			database.conn = database_open(log, dbpath, SQLITE_OPEN_READWRITE);
+
+			if (!database.conn) {
+				return 10;
+			}
+
+			int status = sqlite_add_popd(log, database.conn, argv[i + 1]);
+			
+			sqlite3_close(database.conn);
+			return status;
+
+		}  else if (i + 1 < argc && !strcmp(argv[i], "delete")) {
+			database.conn = database_open(log, dbpath, SQLITE_OPEN_READWRITE);
+
+			if (!database.conn) {
+				return 10;
+			}
+
+			status = sqlite_delete_popd(log, database.conn, argv[i + 1]);
+			sqlite3_close(database.conn);
+			return status;
+		}  else if (i + 1 < argc && !strcmp(argv[i], "unlock")) {
+			database.conn = database_open(log, dbpath, SQLITE_OPEN_READWRITE);
+
+			if (!database.conn) {
+				return 10;
+			}
+			
+			status = sqlite_update_popd(log, database.conn, argv[i + 1], 0);
+			sqlite3_close(database.conn);
+			return status;
+		}  else if (!strcmp(argv[i], "list")) {
+			database.conn = database_open(log, dbpath, SQLITE_OPEN_READONLY);
+
+			if (!database.conn) {
+				return 10;
+			}
+			if (i + 1 < argc) {
+				status = sqlite_get_popd(log, database.conn, argv[i + 1]);
+			} else {
+				status = sqlite_get_all_popd(log, database.conn);
+			}
+			sqlite3_close(database.conn);
+			return status;
+		}
+	}
+
+	printf("Invalid or no arguments.\n");
+	usage();
+
+	return 0;
+}
---
+++ cmail-admin/popd/popd.c
@@ -0,0 +1,147 @@
+int sqlite_get_popd(LOGGER log, sqlite3* db, const char* username) {
+
+
+	char* sql = "SELECT pop_user, pop_lock FROM popd WHERE pop_user LIKE ?";
+
+	sqlite3_stmt* stmt = database_prepare(log, db, sql);
+
+	if (!stmt) {
+		return 2;
+	}
+
+	if (sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC) != SQLITE_OK) {
+		logprintf(log, LOG_ERROR, "Cannot bind expression.\n");
+		return 3;
+	}
+
+	int status;
+	const char* user;
+	int lock;
+	
+	while ((status = sqlite3_step(stmt)) == SQLITE_ROW) {
+		
+		user = (const char*) sqlite3_column_text(stmt, 0);
+		lock = sqlite3_column_int(stmt, 1);
+		printf("%s | %d\n", user, lock);
+	}
+
+	sqlite3_finalize(stmt);
+	if (status == SQLITE_DONE) {
+		return 0;
+	}
+
+	return status;
+}
+
+int sqlite_get_all_popd(LOGGER log, sqlite3* db) {
+
+	char* sql = "SELECT pop_user, pop_lock FROM popd";
+	sqlite3_stmt* stmt = database_prepare(log, db, sql);
+
+	if (!stmt) {
+		return 2;
+	}
+	int status;
+	const char* user;
+	int lock;
+	
+	while ((status = sqlite3_step(stmt)) == SQLITE_ROW) {
+		
+		user = (const char*) sqlite3_column_text(stmt, 0);
+		lock = sqlite3_column_int(stmt, 1);
+		printf("%s | %d\n", user, lock);
+	}
+
+	
+	sqlite3_finalize(stmt);
+
+	if (status == SQLITE_DONE) {
+		return 0;
+	}
+	return status;
+}
+
+int sqlite_add_popd(LOGGER log, sqlite3* db, const char* user) {
+
+	char* sql = "INSERT INTO popd (pop_user) VALUES (?)";
+
+        sqlite3_stmt* stmt = database_prepare(log, db, sql);
+        if (!stmt) {
+                return 2;
+        }
+
+	if (sqlite3_bind_text(stmt, 1, user , -1, SQLITE_STATIC) != SQLITE_OK) {
+                logprintf(log, LOG_ERROR, "Cannot bind user.\n");
+                sqlite3_finalize(stmt);
+                return 91;
+        }
+	
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+                logprintf(log, LOG_ERROR, "%s\n", sqlite3_errmsg(db));
+                sqlite3_finalize(stmt);
+		return 5;
+        }
+        sqlite3_finalize(stmt);
+        return 0;
+}
+
+int sqlite_update_popd(LOGGER log, sqlite3* db, const char* user, int lock) {
+
+	char* sql = "UPDATE popd SET pop_lock = ? WHERE pop_user = ?";
+
+        sqlite3_stmt* stmt = database_prepare(log, db, sql);
+        if (!stmt) {
+                return 2;
+        }
+
+        if (sqlite3_bind_int(stmt, 1, lock) != SQLITE_OK) {
+		logprintf(log, LOG_ERROR, "Cannot bind popd lock.\n");
+		return 81;
+	}
+	
+	if (sqlite3_bind_text(stmt, 2, user , -1, SQLITE_STATIC) != SQLITE_OK) {
+                logprintf(log, LOG_ERROR, "Cannot bind user.\n");
+                sqlite3_finalize(stmt);
+                return 82;
+        }
+	
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+                logprintf(log, LOG_ERROR, "%s\n", sqlite3_errmsg(db));
+                sqlite3_finalize(stmt);
+		return 5;
+        }
+        sqlite3_finalize(stmt);
+	if (sqlite3_changes(db) < 1) {
+		printf("POPD entry not found.\n");
+	}
+        return 0;
+}
+
+int sqlite_delete_popd(LOGGER log, sqlite3* db, const char* user) {
+
+	char* sql = "DELETE FROM popd WHERE pop_user = ?";
+
+	sqlite3_stmt* stmt = database_prepare(log, db, sql);
+	if (!stmt) {
+		return 2;
+	}
+
+	if (sqlite3_bind_text(stmt, 1, user, -1, SQLITE_STATIC) != SQLITE_OK) {
+		logprintf(log, LOG_INFO, "Cannot bind user.\n");
+		sqlite3_finalize(stmt);
+		return 3;
+	}
+
+	if (sqlite3_step(stmt) != SQLITE_DONE) {
+		logprintf(log, LOG_ERROR, "%s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(stmt);
+		return 5;
+	}
+
+	if (sqlite3_changes(db) < 1) {
+		printf("POPD entry not found.\n");
+	}
+	sqlite3_finalize(stmt);
+
+	return 0;
+}
