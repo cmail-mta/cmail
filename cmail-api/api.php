554333e302c564be1972d2ab4044aa8e1281e0aa
cbdev <cb@cbcdn.com>
2015-06-02 18:34:23 +0200

    raise schema version to 8 in modules

--- cmail-api/api.php
+++ cmail-api/api.php
@@ -52,7 +52,7 @@ function main($module_name) {
 	$output = Output::getInstance();
 	$db = new DB($dbpath, $output);
 
-	$API_VERSION = 7;
+	$API_VERSION = 8;
 
 	// db connection
 	if (!$db->connect()) {
