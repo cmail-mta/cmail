79f5abbae934274ec993d9ee273b497cd8796fe0
cbdev <cb@cbcdn.com>
2015-07-25 17:37:32 +0200

    Beauty fixes

--- cmail-popd/client.c
+++ cmail-popd/client.c
@@ -1,5 +1,5 @@
 int client_line(LOGGER log, CONNECTION* client, DATABASE* database){
-	CLIENT* client_data=(CLIENT*)client->aux_data;
+	CLIENT* client_data = (CLIENT*)client->aux_data;
 
 	logprintf(log, LOG_ALL_IO, ">> %s\n", client_data->recv_buffer);
 
@@ -43,30 +43,30 @@ int client_accept(LOGGER log, CONNECTION* listener, CONNPOOL* clients){
 		}
 	};
 	CLIENT* actual_data;
-	LISTENER* listener_data=(LISTENER*)listener->aux_data;
+	LISTENER* listener_data = (LISTENER*)listener->aux_data;
 
-	if(connpool_active(*clients)>=CMAIL_MAX_CONCURRENT_CLIENTS){
+	if(connpool_active(*clients) >= CMAIL_MAX_CONCURRENT_CLIENTS){
 		logprintf(log, LOG_INFO, "Not accepting new client, limit reached\n");
 		return 1;
 	}
 
-	client_slot=connpool_add(clients, accept(listener->fd, NULL, NULL));
+	client_slot = connpool_add(clients, accept(listener->fd, NULL, NULL));
 
-	if(client_slot<0){
+	if(client_slot < 0){
 		logprintf(log, LOG_ERROR, "Failed to pool client socket\n");
 		return -1;
 	}
 
 	//set socket nonblocking
-	flags=fcntl(clients->conns[client_slot].fd, F_GETFL, 0);
-	if(flags<0){
+	flags = fcntl(clients->conns[client_slot].fd, F_GETFL, 0);
+	if(flags < 0){
 		flags=0;
 	}
 	//FIXME check errno
-  	fcntl(clients->conns[client_slot].fd, F_SETFL, flags|O_NONBLOCK);
+	fcntl(clients->conns[client_slot].fd, F_SETFL, flags | O_NONBLOCK);
 
 	if(!(clients->conns[client_slot].aux_data)){
-		clients->conns[client_slot].aux_data=malloc(sizeof(CLIENT));
+		clients->conns[client_slot].aux_data = malloc(sizeof(CLIENT));
 		if(!clients->conns[client_slot].aux_data){
 			logprintf(log, LOG_ERROR, "Failed to allocate client data set\n");
 			return -1;
@@ -74,18 +74,18 @@ int client_accept(LOGGER log, CONNECTION* listener, CONNPOOL* clients){
 	}
 	else{
 		//preserve old data
-		//TODO
+		//none
 	}
 
 	//initialise / reset client data structure
-	actual_data=(CLIENT*)clients->conns[client_slot].aux_data;
+	actual_data = (CLIENT*)clients->conns[client_slot].aux_data;
 	*actual_data = empty_data;
 
 	#ifndef CMAIL_NO_TLS
 	//if on tlsonly port, immediately wait for negotiation
-	if(listener->tls_mode==TLS_ONLY){
+	if(listener->tls_mode == TLS_ONLY){
 		logprintf(log, LOG_INFO, "Listen socket is TLSONLY, waiting for negotiation...\n");
-		clients->conns[client_slot].tls_mode=TLS_NEGOTIATE;
+		clients->conns[client_slot].tls_mode = TLS_NEGOTIATE;
 		return tls_init_serverpeer(log, &(clients->conns[client_slot]), listener_data->tls_priorities, listener_data->tls_cert);
 	}
 	#endif
@@ -172,7 +172,7 @@ int client_process(LOGGER log, CONNECTION* client, DATABASE* database){
 			break;
 			case TLS_NEGOTIATE:
 				//errors during TLS negotiation
-				if(bytes==-2){
+				if(bytes == -2){
 					client_close(log, client, database);
 				}
 				return 0;
@@ -192,13 +192,13 @@ int client_process(LOGGER log, CONNECTION* client, DATABASE* database){
 	}
 
 	//client disconnect / handshake success
-	else if(bytes==0){
+	else if(bytes == 0){
 		#ifndef CMAIL_NO_TLS
 		switch(client->tls_mode){
 			case TLS_NEGOTIATE:
 				//tls handshake ok
-				client->tls_mode=TLS_ONLY;
-				if(client_data->listener->tls_mode==TLS_ONLY){
+				client->tls_mode = TLS_ONLY;
+				if(client_data->listener->tls_mode == TLS_ONLY){
 					//send greeting if listener is tlsonly
 					client_send(log, client, "+OK %s POP3 ready\r\n", listener_data->announce_domain);
 				}
@@ -216,9 +216,9 @@ int client_process(LOGGER log, CONNECTION* client, DATABASE* database){
 	logprintf(log, LOG_DEBUG, "Received %d bytes of data, recv_offset is %d\n", bytes, client_data->recv_offset);
 
 	do{
-		line_length=common_next_line(log, client_data->recv_buffer, &(client_data->recv_offset), &bytes);
-		if(line_length>=0){
-			if(line_length>=POP_MAX_LINE_LENGTH-2){
+		line_length = common_next_line(log, client_data->recv_buffer, &(client_data->recv_offset), &bytes);
+		if(line_length >= 0){
+			if(line_length >= POP_MAX_LINE_LENGTH-2){
 				logprintf(log, LOG_WARNING, "Line too long, ignoring\n");
 				client_send(log, client, "-ERR Line too long\r\n");
 				//client_line(log, client, database);
@@ -232,7 +232,7 @@ int client_process(LOGGER log, CONNECTION* client, DATABASE* database){
 			}
 		}
 	}
-	while(line_length>=0);
+	while(line_length >= 0);
 
 	#ifndef CMAIL_NO_TLS
 	}
