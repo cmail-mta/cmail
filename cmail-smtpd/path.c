e40e84167b36a6bd4efab135e4922dda556772b6
cbdev <cb@cbcdn.com>
2015-08-21 16:27:41 +0200

    Implement path parsing comment detection

--- cmail-smtpd/path.c
+++ cmail-smtpd/path.c
@@ -1,7 +1,7 @@
 //TODO test this thoroughly
 int path_parse(LOGGER log, char* pathspec, MAILPATH* path){
 	//See http://cr.yp.to/smtp/address.html for hints on address parsing
-	bool quotes = false, done_parsing = false;
+	bool quotes = false, done_parsing = false, comment = false;
 	unsigned out_pos = 0, in_pos = 0;
 
 	//skip leading spaces
@@ -11,68 +11,86 @@ int path_parse(LOGGER log, char* pathspec, MAILPATH* path){
 	logprintf(log, LOG_DEBUG, "Parsing path %s\n", pathspec);
 
 	for(in_pos = 0; !done_parsing && out_pos < (SMTP_MAX_PATH_LENGTH - 1) && pathspec[in_pos]; in_pos++){
-		switch(pathspec[in_pos]){
-			case '@':
-				if(out_pos == 0){
-					//route syntax. skip until next colon.
-					for(;pathspec[in_pos] && pathspec[in_pos]!=':';in_pos++){
+		if(!comment){
+			switch(pathspec[in_pos]){
+				case '@':
+					if(out_pos == 0){
+						//route syntax. skip until next colon.
+						for(;pathspec[in_pos] && pathspec[in_pos]!=':';in_pos++){
+						}
+						if(pathspec[in_pos] != ':'){
+							//colon was somehow the last character. someone blew this.
+							done_parsing = true;
+						}
 					}
-					if(pathspec[in_pos] != ':'){
-						//colon was somehow the last character. someone blew this.
+					else{
+						//copy to out buffer and update delimiter position
+						path->delimiter_position = out_pos;
+						path->path[out_pos++] = pathspec[in_pos];
+					}
+					break;
+				case '"':
+					quotes = !quotes;
+					break;
+				case '\\':
+					//escape character. add next char to out buffer
+					//WARNING this can cause an issue when implemented incorrectly.
+					//if the last character sent is a backslash escaping \0 the
+					//loop potentially accesses memory out of bounds.
+					//so, we check for that.
+					//actually, in this implementation, there are at least
+					//2 \0 bytes in that case, so this is a non-issue.
+					//FIXME allow only printable/space characters here
+					if(pathspec[in_pos + 1]){
+						in_pos++;
+						if(isprint(pathspec[in_pos])){
+							path->path[out_pos++] = pathspec[in_pos];
+						}
+					}
+					else{
 						done_parsing = true;
+						break;
 					}
-				}
-				else{
-					//copy to out buffer and update delimiter position
-					path->delimiter_position = out_pos;
-					path->path[out_pos++] = pathspec[in_pos];
-				}
-				break;
-			case '"':
-				quotes = !quotes;
-				break;
-			case '\\':
-				//escape character. add next char to out buffer
-				//WARNING this can cause an issue when implemented incorrectly.
-				//if the last character sent is a backslash escaping \0 the
-				//loop potentially accesses memory out of bounds.
-				//so, we check for that.
-				//actually, in this implementation, there are at least
-				//2 \0 bytes in that case, so this is a non-issue.
-				//FIXME allow only printable/space characters here
-				if(pathspec[in_pos + 1]){
-					in_pos++;
+					break;
+				case '(':
+					//comment delimiter
+					comment = true;
+					break;
+				case ')':
+					//comment closed without active comment context
+					logprintf(log, LOG_WARNING, "Path contained illegal parenthesis\n");
+					return -1;
+				case '<':
+					if(!quotes){
+						//start mark. ignore it.
+						break;
+					}
+					//fall through
+				case '>':
+					//FIXME allow only printable nonspace(?) characters here
+					if(!quotes){
+						done_parsing = true;
+						break;
+					}
+					//fall through
+				default:
+					//copy to out buffer
 					if(isprint(pathspec[in_pos])){
 						path->path[out_pos++] = pathspec[in_pos];
 					}
-				}
-				else{
-					done_parsing = true;
-					break;
-				}
-				break;
-			case '<':
-				if(!quotes){
-					//start mark. ignore it.
-					break;
-				}
-				//fall through
-			case '>':
-				//FIXME allow only printable nonspace(?) characters here
-				if(!quotes){
-					done_parsing = true;
-					break;
-				}
-				//fall through
-			default:
-				//copy to out buffer
-				if(isprint(pathspec[in_pos])){
-					path->path[out_pos++] = pathspec[in_pos];
-				}
+			}
+		}
+		else if(pathspec[in_pos] == ')'){
+			comment = false;
 		}
 	}
 
 	path->path[out_pos] = 0;
+
+	if(comment){
+		logprintf(log, LOG_WARNING, "Path contained an unterminated comment\n");
+		return -1;
+	}
 
 	logprintf(log, LOG_DEBUG, "Result is %s, delimiter is at %d\n", path->path, path->delimiter_position);
 	return 0;
