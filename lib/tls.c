60805df9801cb601680ad69c44d02e193dbbfc27
cbdev <cb@cbcdn.com>
2016-11-14 18:31:08 +0100

    Beauty fixes

--- lib/tls.c
+++ lib/tls.c
@@ -22,7 +22,7 @@ int tls_handshake(LOGGER log, CONNECTION* conn){
 	int status;
 
 	do{
-		status=gnutls_handshake(conn->tls_session);
+		status = gnutls_handshake(conn->tls_session);
 		if(status){
 			if(gnutls_error_is_fatal(status)){
 				logprintf(log, LOG_WARNING, "Handshake failed: %s\n", gnutls_strerror(status));
@@ -34,32 +34,32 @@ int tls_handshake(LOGGER log, CONNECTION* conn){
 	while(status && !gnutls_error_is_fatal(status));
 	logprintf(log, LOG_INFO, "TLS Handshake succeeded\n");
 
-	conn->tls_mode=TLS_ONLY;
+	conn->tls_mode = TLS_ONLY;
 	return 0;
 }
 
 int tls_init_clientpeer(LOGGER log, CONNECTION* conn, char* remote, gnutls_certificate_credentials_t credentials){
 	int status;
 
-	status=gnutls_init(&(conn->tls_session), GNUTLS_CLIENT);
+	status = gnutls_init(&(conn->tls_session), GNUTLS_CLIENT);
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to initialize TLS session for client: %s\n", gnutls_strerror(status));
 		return -1;
 	}
 
-	status=gnutls_server_name_set(conn->tls_session, GNUTLS_NAME_DNS, remote, strlen(remote));
+	status = gnutls_server_name_set(conn->tls_session, GNUTLS_NAME_DNS, remote, strlen(remote));
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to update TLS server name: %s\n", gnutls_strerror(status));
 		return -1;
 	}
 
-	status=gnutls_credentials_set(conn->tls_session, GNUTLS_CRD_CERTIFICATE, credentials);
+	status = gnutls_credentials_set(conn->tls_session, GNUTLS_CRD_CERTIFICATE, credentials);
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to update TLS credentials: %s\n", gnutls_strerror(status));
 		return -1;
 	}
 
-	status=gnutls_set_default_priority(conn->tls_session); //FIXME this should probably be configurable
+	status = gnutls_set_default_priority(conn->tls_session); //FIXME this should probably be configurable
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to update client TLS priorities: %s\n", gnutls_strerror(status));
 		return -1;
@@ -75,18 +75,18 @@ int tls_init_clientpeer(LOGGER log, CONNECTION* conn, char* remote, gnutls_certi
 int tls_init_serverpeer(LOGGER log, CONNECTION* client, gnutls_priority_t tls_priorities, gnutls_certificate_credentials_t tls_cert){
 	int status;
 
-	status=gnutls_init(&(client->tls_session), GNUTLS_SERVER);
+	status = gnutls_init(&(client->tls_session), GNUTLS_SERVER);
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to initialize TLS session for client: %s\n", gnutls_strerror(status));
 		return -1;
 	}
 
-	status=gnutls_priority_set(client->tls_session, tls_priorities);
+	status = gnutls_priority_set(client->tls_session, tls_priorities);
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to update priority set for client: %s\n", gnutls_strerror(status));
 	}
 
-	status=gnutls_credentials_set(client->tls_session, GNUTLS_CRD_CERTIFICATE, tls_cert);
+	status = gnutls_credentials_set(client->tls_session, GNUTLS_CRD_CERTIFICATE, tls_cert);
 	if(status){
 		logprintf(log, LOG_WARNING, "Failed to set credentials for client: %s\n", gnutls_strerror(status));
 		return -1;
@@ -104,7 +104,7 @@ int tls_init_listener(LOGGER log, LISTENER* listener, char* cert, char* key, cha
 		uint8_t* file_buffer = NULL;
 
 		logprintf(log, LOG_DEBUG, "Initializing TLS priorities\n");
-		if(gnutls_priority_init(&(listener->tls_priorities), (priorities)?priorities:"NORMAL", NULL)){
+		if(gnutls_priority_init(&(listener->tls_priorities), (priorities) ? priorities:"NORMAL", NULL)){
 			logprintf(log, LOG_ERROR, "Failed to initialize TLS priorities\n");
 			return -1;
 		}
